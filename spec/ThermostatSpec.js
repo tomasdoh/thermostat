de808d4209d0feb958866932e9956f663a13fc45
Tomas Doherty <tomasdoh1@gmail.com>
2018-11-28 15:53:50 +0000

    Introduced temperatureUp, temperatureDown and powerSavingOn functions - all tests passing

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -5,12 +5,36 @@ describe('Thermostat', function() {
   var thermostat;
 
   beforeEach(function(){
-    thermostat = new Thermostat;
+    thermostat = new Thermostat();
   });
 
 // Thermostat starts at 20 degrees
   it('has a default temperature of 20 degrees', function(){
-    expect(thermostat._DEFAULTTEMP).toEqual(20);
+    expect(thermostat.temperature).toEqual(20);
+  });
+
+// You can increase the temperature with an up function
+  it('the temperature can be increased', function(){
+    thermostat.temperatureUp();
+    expect(thermostat.currentTemperature()).toEqual(21);
+  });
+
+// You can decrease the temperature with an down function
+  it('the temperature can be decreased', function(){
+    thermostat.temperatureDown();
+    expect(thermostat.currentTemperature()).toEqual(19);
+  });
+
+// The minimum temperature is 10 degrees
+  it('the minimum temperature is 10 degrees', function(){
+    thermostat.temperature = 10
+    expect(function(){ thermostat.temperatureDown(); }).toThrowError('minimum temp exceeded');
+  });
+
+// If power saving mode is on, the maximum temperature is 25 degrees
+  it('power saving mode can be turned on or off', function(){
+    thermostat.powerSavingOn();
+    expect(thermostat.powerSaving).toBe(true);
   });
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,5 +1,26 @@
 'use strict';
 
-function Thermostat () {
-  this._DEFAULTTEMP = 20;
+function Thermostat() {
+  this.temperature = 20;
+  this.MINIMUMTEMP = 10;
+  this.powerSaving = false;
+};
+
+Thermostat.prototype.currentTemperature = function() {
+  return this.temperature;
+};
+
+Thermostat.prototype.temperatureUp = function() {
+  this.temperature += 1;
+};
+
+Thermostat.prototype.temperatureDown = function() {
+  this.temperature -= 1
+  if (this.temperature < this.MINIMUMTEMP)
+    { throw new Error ("minimum temp exceeded");
+  };
+};
+
+Thermostat.prototype.powerSavingOn = function() {
+  this.powerSaving = true;
 };
